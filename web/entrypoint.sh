98e511c752ef6b39d08f99966241d71363cb15aa
Joshua Taylor <rpgplayer1@yahoo.com>
2016-01-16 17:26:24 -0500

    drupal can now create and secure its own permissions file for the default site

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -4,23 +4,31 @@
 if [ ! -d /var/www/html ]
   then
 
-echo wtf
+# Set working directory
+cd /var/www/
 
 # Move Drupal to html
 # This is a bit tricky because of a problem in AUFS: https://github.com/docker/docker/issues/4570
 shopt -s dotglob
-rm -r /var/www/html
-mv /root/drupal /var/www/html
+rm -r ./html
+mv /root/drupal ./html
 shopt -u dotglob
 
 # Settings.php and default files stuff - this really shouldn't be needed! How can I fix that?
-cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
-chmod g+w /var/www/html/sites/default/settings.php
-chmod g+w /var/www/html/sites/default
+#cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
+#chmod g+w /var/www/html/sites/default/settings.php
+#chmod g+w /var/www/html/sites/default
 
 # Set proper Apache permissions
-chown -R root:www-data /var/www
-chmod -R g+s /var/www
+chown -R root:www-data .
+chmod -R g+s .
+
+# Set proper Drupal permissions
+cd /var/www/html
+find . -type d -exec chmod u=rwx,g=rx,o= '{}' \;
+find . -type f -exec chmod u=rw,g=r,o= '{}' \;
+chown www-data:www-data ./sites/default
+chown www-data:www-data ./sites/default/default.settings.php
 
 # Put the default virtual host in place
 # It was wiped out when we mounted /etc/apache2/sites-enabled as a volume
